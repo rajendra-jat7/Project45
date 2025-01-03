7ad26785de66abe7115f0dc35f700a7d3975ad7b
Rajendra Jat <jatrj18@gmail.com>
2025-01-03 07:45:17 +0530

    Day 2 : Personal-Bio

---
+++ personal-bio/src/components/PersonalBio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './PersonalBio.css';
+import Profile from '../assets/Profile.jpg'
+
+const PersonalBio = ({ name, age, profession, bio, image }) => {
+  return (
+    <div className="bio-card">
+      <img src={Profile} alt='Profile' className="bio-image" />
+      <h2 className="bio-name">{name}</h2>
+      <p className="bio-detail"><strong>Age:</strong> {age}</p>
+      <p className="bio-detail"><strong>Profession:</strong> {profession}</p>
+      <p className="bio-description">{bio}</p>
+    </div>
+  );
+};
+
+export default PersonalBio;
